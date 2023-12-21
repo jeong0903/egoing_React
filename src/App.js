69884d475541f16c5c1b5ab58b56aaf817bf676a
Jane Park <9193jane@gmail.com>
2023-12-21 22:50:35 +0900

    20231221 2-6 components

--- src/App.js
+++ src/App.js
@@ -1,48 +1,17 @@
 import React, { Component } from "react";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 import "./App.css";
 
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>WEB</h1>
-        world wide web!
-      </header>
-    );
-  }
-}
-
-class TOC extends Component{
-  render() {
-    return (
-      <nav>
-      <ul>
-        <li><a href="1.html">HTML</a></li>
-        <li><a href="2.html">CSS</a></li>
-        <li><a href="3.html">Java Script</a></li>
-      </ul>
-    </nav>
-    )
-  }
-}
-
-class Content extends Component {
-  render() {
-    return(
-      <article>
-      <h2>HTML</h2>
-      HTML is Hyper Text Mark up Languege.
-    </article>
-    )
-  }
-}
 class App extends Component {
   render() {
     return (
     <div className="App">Hello React 💁🏻‍♀️
-      <Subject></Subject>
+      <Subject title="WEB" sub="world wide web!!"></Subject>
+      <Subject title="React" sub="For UI"></Subject>
       <TOC></TOC>
-      <Content></Content>
+      <Content title ="HTML" desc="HTML is Hyper Text Mark up Languege."></Content>
     </div>
     );
   }
