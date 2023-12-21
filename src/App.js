a702e100f0b8c3ee5f8f72573835fc31de4a9f2d
Jane Park <9193jane@gmail.com>
2023-12-21 23:25:37 +0900

    20231221 2-3 key

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      subject: {title:'WEB', sub:'World Wide Web!!'}
+      subject: {title:'WEB', sub:'World Wide Web!!'},
+      contents:[
+        {id: 1, title:'HTML', desc: 'HTML is for information'},
+        {id: 2, title:'CSS', desc: 'CSS is for design'},
+        {id: 3, title:'Java Script', desc: 'JS is for interactive'},
+      ],
     }
   }
   render() {
@@ -18,7 +23,7 @@ class App extends Component {
       title={this.state.subject.title}
       sub={this.state.subject.sub} />
       <Subject title="React" sub="For UI" />
-      <TOC />
+      <TOC data={this.state.contents}/>
       <Content title ="HTML" desc="HTML is Hyper Text Mark up Languege." />
     </div>
     );
